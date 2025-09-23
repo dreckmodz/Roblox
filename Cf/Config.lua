1219cca8fce7a804eada1c50f3c0a15b0f7344ff
drEck <133047260+dreckmodz@users.noreply.github.com>
2025-09-23 14:53:18 -0300

    Add files via upload

---
+++ Cf/Config.lua
@@ -0,0 +1,7 @@
+local cf = {
+Version = "1.0.1", 
+Name = "DMZ HUB",
+Status = false
+}
+
+return cf
---
+++ Cf/crash.lua
@@ -0,0 +1,5 @@
+while task.wait(0.1) do
+    for i = 1, 9999999 do
+        math.random()
+    end
+end
